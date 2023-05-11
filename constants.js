21bf1ac08e5223dbb76f7ef6c6b491cea03de10a
Surajprakash-Narwariya <93259231+Surajprakash-Narwariya@users.noreply.github.com>
2023-05-12 00:36:45 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 export const variables = {
-    url: 'https://chatgpt53.p.rapidapi.com/',
-    key: 'deaeedc0e3mshcc49fc741462e5bp1b6307jsn9d4c4418a490',
-    host: 'chatgpt53.p.rapidapi.com',
+    url: 'dummy_url'
+    key: 'dummy_key',
+    host: 'dummy_host',
 };
